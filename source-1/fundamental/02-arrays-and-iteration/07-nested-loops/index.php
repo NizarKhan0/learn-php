d7f9eb92984a116ffd2a73232577e4abdfd8cfc4
nizarkhan0 <nizarkhan7071@gmail.com>
2025-01-29 23:41:41 +0800

    nested loops

--- source-1/fundamental/02-arrays-and-iteration/07-nested-loops/index.php
+++ source-1/fundamental/02-arrays-and-iteration/07-nested-loops/index.php
@@ -1,3 +1,27 @@
+<?php
+
+  // for ($i = 0; $i < 5; $i++) {
+  //   for ($j = 0; $j < 5; $j++) {
+  //     echo $i . ' - ' . $j . '<br>';
+  //   }
+  // }
+
+// $i = 0;
+
+// while ($i < 5) {
+//   $j = 0;
+
+//   while ($j < 5) {
+//     echo $i . ' - ' . $j . '<br>';
+//     $j++;
+//   }
+
+//   $i++;
+// }
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +30,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Nested Loops & CSS Grid Example</title>
   <style>
+    body{
+      background-color: rebeccapurple;
+    }
+
     .grid-container {
       display: grid;
       grid-template-columns: repeat(5, 50px);
@@ -26,6 +54,13 @@
 
 <body>
   <div class="grid-container">
+    <?php for ($i = 0; $i < 5; $i++) : ?>
+      <?php for ($j = 0; $j < 5; $j++) : ?>
+        <div class="grid-item">
+          <?php echo $i . ' - ' . $j; ?>
+        </div>
+        <?php endfor; ?>
+      <?php endfor; ?>
   </div>
 </body>
 
