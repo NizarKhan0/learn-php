7f20efa1a5ba4ed08e0ace9a244d5ff18a4274e3
nizarkhan0 <nizarkhan7071@gmail.com>
2025-01-29 23:09:10 +0800

    basic loops

--- source-1/fundamental/02-arrays-and-iteration/06-basic-loops/index.php
+++ source-1/fundamental/02-arrays-and-iteration/06-basic-loops/index.php
@@ -1,3 +1,28 @@
+<?php
+
+//basic for loop
+// for($i = 0; $i <= 10; $i++) {
+//     echo $i . '<br/>';
+// }
+
+//basic while loop
+// $i = 0;
+
+// while($i < 10) {
+//     echo $i . '<br/>';
+//     $i++;
+// }
+
+//basic do while loop
+// $i = 0;
+
+// do {
+//     echo $i . '<br/>';
+//     $i++;
+// } while ($i < 10);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,14 +34,35 @@
 </head>
 
 <body class="bg-gray-100">
-    <header class="bg-blue-500 text-white p-4">
+    <header class="p-4 text-white bg-blue-500">
         <div class="container mx-auto">
             <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
         </div>
     </header>
-    <div class="container mx-auto p-4 mt-4">
-        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+    <div class="container p-4 mx-auto mt-4">
+        <div class="p-6 mt-6 bg-white rounded-lg shadow-md">
             <!-- Output -->
+            <!-- <ul>
+                <?php for ($i = 0; $i <= 10; $i++) : ?>
+                    <li>Number: <?= $i; ?></li>
+                <?php endfor; ?>
+            </ul> -->
+
+            <!-- <ul>
+                <?php $i = 0;
+                while ($i < 10) : ?>
+                    <li>Number: <?= $i; ?></li>
+                <?php $i++;
+                endwhile; ?>
+            </ul> -->
+
+            <ul>
+                <?php $i = 0; do { ?>
+                    <li>Number: <?= $i; ?></li>
+                <?php $i++;
+                } while ($i < 10); ?>
+            </ul>
+            
         </div>
     </div>
 </body>
