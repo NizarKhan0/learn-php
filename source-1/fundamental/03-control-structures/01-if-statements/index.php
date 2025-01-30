a5856e2f272c78c437fbf9b52f12d08be90ca05c
nizarkhan0 <nizarkhan7071@gmail.com>
2025-01-30 19:59:42 +0800

    if-else-statements

--- source-1/fundamental/03-control-structures/01-if-statements/index.php
+++ source-1/fundamental/03-control-structures/01-if-statements/index.php
@@ -1,10 +1,45 @@
 <?php
 
+$age = 17;
+
+
 // If statement
 
+if($age >=18){
+    echo 'You are old enough to vote';
+};
+
+echo '<br>';
+
 // If-Else
+if($age >=21){
+    echo 'You are old enough to vote';
+}else{
+    echo 'You are NOT old enough to vote';
+}
+
+echo '<br>';
 
 // Nested if statement
+if($age >=21){
+    echo 'You are old enough to drink and vote';
+}else{
+    if($age >=18){
+        echo 'You are old enough to vote';
+    }else{
+        echo 'You are NOT old enough to drink or vote';
+    }
+}
+
+echo '<br>';
 
 // If-Else-If
+if($age >=21){
+    echo 'You are old enough to drink and vote';
+}else if ($age >=18){
+    echo 'You are old enough to vote';
+}else{
+    echo 'You are NOT old enough to drink or vote';
+}
 
+?>
