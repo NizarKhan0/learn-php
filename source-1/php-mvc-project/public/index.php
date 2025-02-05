1e140d853c2c6d2c6fbc0b2d416e1708029f1f4e
Nizar Khan <nizarkhan7071@gmail.com>
2025-02-06 05:32:24 +0800

    setup project & template

---
+++ source-1/php-mvc-project/public/index.php
@@ -0,0 +1,10 @@
+<?php
+    // echo 'Hello World';
+
+    // ini dah ganti guna helper
+    // require '../views/home-view.php';
+
+    require '../helpers.php';
+
+    require basePath('views/home-view.php');
+?>
