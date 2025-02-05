6afd2b56de81344622bfe5c3405edf1e105ceeef
Nizar Khan <nizarkhan7071@gmail.com>
2025-02-05 23:27:43 +0800

    db - create, update & delete records

--- source-1/fundamental/07-database/11-delete-records/post.php
+++ source-1/fundamental/07-database/11-delete-records/post.php
@@ -30,29 +30,43 @@ $post = $stmt->fetch();
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title><?= $post['title'] ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title><?= $post['title'] ?></title>
 </head>
 
 <body class="bg-gray-100">
-  <header class="bg-blue-500 text-white p-4">
-    <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">My Blog</h1>
-    </div>
-  </header>
-  <div class="container mx-auto p-4 mt-4">
-    <div class="md my-4">
-      <div class="rounded-lg shadow-md mb-5">
-        <div class="p-4">
-          <h2 class="text-xl font-semibold"><?= $post['title']; ?></h2>
-          <p class="text-gray-700 text-lg mt-2 mb-5"><?= $post['body']; ?></p>
-          <a href="index.php">Go Back</a>
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">My Blog</h1>
         </div>
-      </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="md my-4">
+            <div class="rounded-lg shadow-md mb-5">
+                <div class="p-4">
+                    <h2 class="text-xl font-semibold"><?= $post['title']; ?></h2>
+                    <p class="text-gray-700 text-lg mt-2 mb-5"><?= $post['body']; ?></p>
+                    <a href="index.php">Go Back</a>
+                </div>
+
+                <div class="p-4">
+                    <form action="delete.php" method="post">
+
+                        <input type="hidden" name="_method" value="delete">
+                        <input type="hidden" name="id" value="<?= $post['id'] ?>">
+
+                        <button type="submit" name="submit"
+                            class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none">
+                            Delete
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
     </div>
-  </div>
 </body>
 
 </html>
--- source-1/fundamental/07-database/12-update-record/post.php
+++ source-1/fundamental/07-database/12-update-record/post.php
@@ -30,34 +30,40 @@ $post = $stmt->fetch();
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title><?= $post['title'] ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title><?= $post['title'] ?></title>
 </head>
 
 <body class="bg-gray-100">
-  <header class="bg-blue-500 text-white p-4">
-    <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">My Blog</h1>
-    </div>
-  </header>
-  <div class="container mx-auto p-4 mt-4">
-    <div class="md my-4">
-      <div class="rounded-lg shadow-md mb-5">
-        <div class="p-4">
-          <h2 class="text-xl font-semibold"><?= $post['title']; ?></h2>
-          <p class="text-gray-700 text-lg mt-2 mb-5"><?= $post['body']; ?></p>
-          <a href="index.php">Go Back</a>
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">My Blog</h1>
         </div>
-      </div>
-      <form action="delete.php" method="post">
-        <input type="hidden" name="_method" value="delete">
-        <input type="hidden" name="id" value="<?= $post['id'] ?>">
-        <button type="submit" name="submit" class="text-xl bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none">Delete</button>
-      </form>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="md my-4">
+            <div class="rounded-lg shadow-md mb-5">
+                <div class="p-4">
+                    <h2 class="text-xl font-semibold"><?= $post['title']; ?></h2>
+                    <p class="text-gray-700 text-lg mt-2 mb-5"><?= $post['body']; ?></p>
+                    <a href="index.php">Go Back</a>
+                </div>
+            </div>
+
+            <a href="edit.php?id=<?= $post['id'] ?>" class="bg-green-500 text-xl text-white px-4 py-2 rounded block w-full text-center mb-4 hover:
+                bg-green-600 focus:outline-none">Edit</a>
+
+
+            <form action="delete.php" method="post">
+                <input type="hidden" name="_method" value="delete">
+                <input type="hidden" name="id" value="<?= $post['id'] ?>">
+                <button type="submit" name="submit"
+                    class="w-full text-xl bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none">Delete</button>
+            </form>
+        </div>
     </div>
-  </div>
 </body>
 
 </html>
